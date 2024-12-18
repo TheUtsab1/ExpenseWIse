32e755b7045b065c2eedfce12cc4439ce7a319df
Utsab Devkota <info.utsab10@gmail.com>
2024-12-18 08:25:55 +0545

    first commit

---
+++ ExpenseWise/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseWise
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExpenseWise/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExpenseWise
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
