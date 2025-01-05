1c5839675ce0ab130b91b01807c1e524f99c15a4
Utsab Devkota <info.utsab10@gmail.com>
2025-01-05 12:32:12 +0545

    ExpenseWise2

---
+++ ExpenseWise2/Model/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseWise2.Models
+{
+
+    public class Transaction1
+    {
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Tags { get; set; }
+        public string Note { get; set; }
+    }
